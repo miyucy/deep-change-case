a0417838868be5def09d82e2164ad90539d36f19
miyucy <fistfvck@gmail.com>
2017-09-05 12:52:11 +0900

    init

---
+++ test.js
@@ -0,0 +1,22 @@
+const assert = require("assert");
+const { camelToSnake, snakeToCamel } = require("./index.js");
+
+[
+  { a: { foo: "foo" }, e: { foo: "foo" } },
+  { a: { fooBar: "foo" }, e: { foo_bar: "foo" } },
+  { a: { fooBarBaz: "foo" }, e: { foo_bar_baz: "foo" } },
+  { a: [1, { two: 2 }, 3], e: [1, { two: 2 }, 3] },
+  { a: [1, { intNum: 2 }, 3], e: [1, { int_num: 2 }, 3] }
+].forEach(testcase => {
+  assert.deepEqual(camelToSnake(testcase.a), testcase.e);
+});
+
+[
+  { a: { foo: "foo" }, e: { foo: "foo" } },
+  { a: { foo_bar: "foo" }, e: { fooBar: "foo" } },
+  { a: { foo_bar_baz: "foo" }, e: { fooBarBaz: "foo" } },
+  { a: [1, { two: 2 }, 3], e: [1, { two: 2 }, 3] },
+  { a: [1, { int_num: 2 }, 3], e: [1, { intNum: 2 }, 3] }
+].forEach(testcase => {
+  assert.deepEqual(snakeToCamel(testcase.a), testcase.e);
+});
